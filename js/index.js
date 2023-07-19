01d847fe189ecdb17498490f1a4f65460284d8b0
LilitVardanyan1992 <lvardanyan890@gmail.com>
2023-07-19 00:37:06 -0700

    editted index.html file and deleted menu card items thend added from JavaScr ipt Class

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", function () {
-    //tab logic start
+    // tab logic start
 
     const tabsHeader = document.querySelectorAll(".tabheader__item");
     const tabsContents = document.querySelectorAll(".tabcontent");
@@ -33,9 +33,9 @@ window.addEventListener("DOMContentLoaded", function () {
         }
     })
 
-    //tab logic end
+    // tab logic end
 
-    //timer logic start
+    // timer logic start
 
     const deadline = "2023-07-17";
 
@@ -45,9 +45,9 @@ window.addEventListener("DOMContentLoaded", function () {
 
         if (total <= 0) {
             days = 0,
-            hours = 0,
-            minutes = 0,
-            seconds = 0
+                hours = 0,
+                minutes = 0,
+                seconds = 0
         } else {
             days = Math.floor(total / (1000 * 60 * 60 * 24));
             hours = Math.floor((total / (1000 * 60 * 60)) % 24);
@@ -72,7 +72,7 @@ window.addEventListener("DOMContentLoaded", function () {
         const timerParts = timer.querySelectorAll("span");
 
         updateClock();
-        
+
         function updateClock() {
             const calculation = getTimeRemaining(deadline);
             const calculationValues = Object.values(calculation);
@@ -91,9 +91,9 @@ window.addEventListener("DOMContentLoaded", function () {
 
     setClock(".timer", deadline);
 
-    //timer logic end
+    // timer logic end
 
-    //modal logic start
+    // modal logic start
 
     const modalTrigger = document.querySelectorAll("[data-modal]");
     const modal = document.querySelector(".modal");
@@ -102,18 +102,18 @@ window.addEventListener("DOMContentLoaded", function () {
     modalTrigger.forEach(btn => btn.addEventListener("click", openModal));
 
     document.addEventListener("click", (e) => {
-        if(e.target !== ".modal" && e.target.matches(".show") || e.target === modalCloseBtn) {
-           closeModal();
+        if (e.target !== ".modal" && e.target.matches(".show") || e.target === modalCloseBtn) {
+            closeModal();
         }
     })
 
     document.addEventListener("keydown", (e) => {
         if (e.code === "Escape") {
-           closeModal();
+            closeModal();
         }
     })
-  
-    const modalTimerId = setTimeout(openModal, 5000);
+
+    // const modalTimerId = setTimeout(openModal, 5000);
 
     function showModalByScroll() {
         if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -124,19 +124,83 @@ window.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener("scroll", showModalByScroll);
 
-	function openModal() {
-		modal.classList.add("show");
-		modal.classList.remove("hide");
-		document.body.style.overflow = "hidden";
-		clearTimeout(modalTimerId);
-	}
+    function openModal() {
+        modal.classList.add("show");
+        modal.classList.remove("hide");
+        document.body.style.overflow = "hidden";
+        clearTimeout(modalTimerId);
+    }
 
-	function closeModal() {
-		modal.classList.add("hide");
-		modal.classList.remove("show");
-		document.body.removeAttribute("style");
-	}
+    function closeModal() {
+        modal.classList.add("hide");
+        modal.classList.remove("show");
+        document.body.removeAttribute("style");
+    }
 
-    //modal logic end
+    // modal logic end
+
+    // used Class for menu items start
+
+    class MenuCard {
+        constructor(img, alt, title, descr, price, parentSelector) {
+            this.img = img;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const {img, alt, title, descr, price, parent} = this;
+            const element = document.createElement("div");
+            element.classList.add("menu__item");
+            element.innerHTML = `
+                    <img src=${img} alt=${alt}>
+                    <h3 class="menu__item-subtitle">${title}</h3>
+                    <div class="menu__item-descr">${descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${price}</span> грн/день</div>
+                    </div>
+            `
+            parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        "Меню \"Фитнес\"",
+        "Меню \"Фитнес\" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+        8,
+        ".menu .container"
+    ).render()
+    
+    new MenuCard(
+		"img/tabs/elite.jpg",
+		"elite",
+		"Меню \"Премиум\"",
+		"В меню \"Премиум\" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+		15,
+		".menu .container"
+	).render();
+
+	new MenuCard(
+		"img/tabs/post.jpg",
+		"postnoe",
+		"Меню \"Постное\"",
+		"Меню \"Постное\" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+		13,
+		".menu .container"
+	).render();
+    // used Class for menu items end
 })
 
