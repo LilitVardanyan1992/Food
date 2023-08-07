d6c674a6f33f7f53e0d691efcfc180f96d199012
LilitVardanyan1992 <lvardanyan890@gmail.com>
2023-08-07 00:56:03 -0700

    added regexp in our project

--- js/index.js
+++ js/index.js
@@ -324,29 +324,48 @@ window.addEventListener("DOMContentLoaded", function () {
 			form.append(loader);
 
 			if (!navigator.onLine) {
-				showThanksModal(failure + ":" + "Please check your internet connection !");
+				messagesModal(failure + ":" + "Please check your internet connection !");
 				loader.remove();
 				form.reset();
 			}
 
-			const formData = new FormData(form);
-			// const json = JSON.parse(JSON.stringify(Object.fromEntries(formData.entries())));
-			const data = Object.fromEntries(formData)
+			const empty = /^$/g;
+			const phone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+			let status = false;
 
-			axios.post("http://localhost:8888/requests", data)
-
-				// postData("http://localhost:8888/requests", json)
-				.then(data => {
-					console.log(data);
-					messagesModal(success);
-				})
-				.catch(err => {
-					messagesModal(failure + ": " + err);
-				})
-				.finally(() => {
+			for (let i = 0; i < form.querySelectorAll("input").length; i++) {
+				if (empty.test(form[i].value) || !phone.test(form[1].value)) {
+					status = false;
+					messagesModal("Please fill all fields, and on Phone number field please fill only numbers")
 					loader.remove();
 					form.reset();
-				});
+					break;
+				} else {
+					status = true;
+				}
+			}
+
+			if (status) {
+				const formData = new FormData(form);
+				// const json = JSON.parse(JSON.stringify(Object.fromEntries(formData.entries())));
+				const data = Object.fromEntries(formData)
+
+				axios.post("http://localhost:8888/requests", data)
+					// postData("http://localhost:8888/requests", json)
+					.then(data => {
+						console.log(data);
+						messagesModal(success);
+					})
+					.catch(err => {
+						messagesModal(failure + ": " + err);
+					})
+					.finally(() => {
+						loader.remove();
+						form.reset();
+					});
+			} else {
+				console.log("status is false");
+			}
 		});
 	}
 
