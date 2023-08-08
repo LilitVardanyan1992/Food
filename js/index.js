0ac0834b018c54d4b45975a52e960db73af5cb60
LilitVardanyan1992 <lvardanyan890@gmail.com>
2023-08-08 03:09:17 -0700

    created calculating calories with localStorage function

--- js/index.js
+++ js/index.js
@@ -409,20 +409,20 @@ window.addEventListener("DOMContentLoaded", function () {
 
 
 	let offset = 0;
-	let slideIndex = 1 
-	const slideWidth = parseFloat(width); 
+	let slideIndex = 1
+	const slideWidth = parseFloat(width);
 
 	function setSlideCountText() {
 		total.textContent = slides.length < 10 ? `0${slides.length}` : slides.length;
 		current.textContent = slideIndex < 10 ? `0${slideIndex}` : slideIndex;
 	}
-	
+
 	function updateSlideAndDot() {
 		slidesField.style.transform = `translateX(-${offset}px)`;
-	
+
 		dots.forEach(dot => dot.style.opacity = "0.5");
 		dots[slideIndex - 1].style.opacity = 1;
-	
+
 		setSlideCountText();
 	}
 
@@ -473,14 +473,14 @@ window.addEventListener("DOMContentLoaded", function () {
 				opacity: .5;
 				transition: opacity .6s ease;
 			`;
-	
+
 			indicators.append(dot);
 			dots.push(dot);
 		}
 
 		return indicators;
 	}
-	
+
 	const dots = [];
 	const indicators = createIndicators();
 	slider.style.position = "relative";
@@ -498,8 +498,8 @@ window.addEventListener("DOMContentLoaded", function () {
 			slideIndex = 1;
 		} else {
 			slideIndex++;
-		} 
-	
+		}
+
 		updateSlideAndDot()
 	})
 
@@ -514,11 +514,11 @@ window.addEventListener("DOMContentLoaded", function () {
 			slideIndex = slides.length;
 		} else {
 			slideIndex--;
-		} 
-        
+		}
+
 		updateSlideAndDot()
 	})
-	
+
 	dots.forEach((dot, index) => {
 		dot.addEventListener("click", (e) => {
 			slideIndex = index + 1;
@@ -557,6 +557,122 @@ window.addEventListener("DOMContentLoaded", function () {
 
 	// SLider varient 1 end
 
+
+	// Calculating start
+	const result = document.querySelector(".calculating__result span");
+
+	let gender = "female";
+	let activityFactor = "small";
+	let height = 0;
+	let weight = 0;
+	let age = 0;
+	
+	// Set up event listeners for static information (gender and activity factor)
+	function setStaticInformation(selector, activeClass, dataKey) {
+	  const elements = document.querySelectorAll(selector);
+	
+	  elements.forEach(elem => {
+		elem.addEventListener("click", () => {
+		  elements.forEach(elem => elem.classList.remove(activeClass));
+		  elem.classList.add(activeClass);
+		  localStorage.setItem(dataKey, elem.id);
+	
+		  if (dataKey === "gender") {
+			gender = elem.id;
+		  } else if (dataKey === "activityFactor") {
+			activityFactor = elem.id;
+		  }
+	
+		  calcTotal();
+		});
+	  });
+	}
+	
+	setStaticInformation("#gender div", "calculating__choose-item_active", "gender");
+	setStaticInformation(".calculating__choose_big div", "calculating__choose-item_active", "activityFactor");
+	
+	// Set up event listeners for dynamic information (height, weight, age)
+	function setDynamicInformation(selector, dataType) {
+	  const input = document.querySelector(selector);
+	
+	  input.addEventListener("input", () => {
+		const value = parseFloat(input.value);
+		if (!isNaN(value)) {
+		  input.style.border = "none";
+	
+		  switch (dataType) {
+			case "height":
+			  height = value;
+			  break;
+			case "weight":
+			  weight = value;
+			  break;
+			case "age":
+			  age = value;
+			  break;
+		  }
+		  
+		  calcTotal();
+		} else {
+		  input.style.border = "1px solid #f00";
+		}
+	  });
+
+	  input.addEventListener("blur", () => {
+		if (isNaN(input.value)) {
+			input.style.border = "1px solid #f00";
+		} else {
+			input.style.border = "none";
+		}
+	  })
+	}
+	
+	setDynamicInformation("#height", "height");
+	setDynamicInformation("#weight", "weight");
+	setDynamicInformation("#age", "age");
+	
+	// Calculate total calories based on the collected data
+	function calcTotal() {
+	  let bmr;
+	
+	  if (!gender || !height || !weight || !age || !activityFactor) {
+		result.textContent = "_________";
+		return;
+	  }
+	
+	  if (gender === "male") {
+		bmr = 88.362 + 13.397 * weight + 4.799 * height - 5.677 * age;
+	  } else if (gender === "female") {
+		bmr = 447.593 + 9.247 * weight + 3.098 * height - 4.33 * age;
+	  } else {
+		throw new Error("Invalid gender");
+	  }
+	
+	  let activityFactorValue;
+	
+	  switch (activityFactor) {
+		case "low":
+		  activityFactorValue = 1.2;
+		  break;
+		case "small":
+		  activityFactorValue = 1.375;
+		  break;
+		case "medium":
+		  activityFactorValue = 1.55;
+		  break;
+		case "high":
+		  activityFactorValue = 1.725;
+		  break;
+		default:
+		  throw new Error("Invalid activity level");
+	  }
+	
+	  const dailyCalories = Math.round(bmr * activityFactorValue);
+	  result.textContent = dailyCalories;
+	}
+	
+	// Initial calculation
+	calcTotal();
 });
 
 
